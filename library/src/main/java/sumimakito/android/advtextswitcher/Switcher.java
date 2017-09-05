08c81f50f69d97fe1c302208a0780131ec29ad69
yueban <fbzhh007@gmail.com>
2017-09-05 14:01:39 +0800

    make weakref to AdvTextSwitcher

--- library/src/main/java/sumimakito/android/advtextswitcher/Switcher.java
+++ library/src/main/java/sumimakito/android/advtextswitcher/Switcher.java
@@ -1,61 +1,50 @@
 package sumimakito.android.advtextswitcher;
 
-import android.content.Context;
 import android.os.Handler;
+import java.lang.ref.WeakReference;
 
-public class Switcher
-{
-	private AdvTextSwitcher advTsView;
-	private boolean isPaused;
-	private int mDuration=1000;
+public class Switcher {
+  public Handler hlUpdt = new Handler();
+  private WeakReference<AdvTextSwitcher> advTsViewRef;
+  private boolean isPaused;
+  private int mDuration = 1000;
+  public Runnable rbUpdt = new Runnable() {
+    @Override
+    public void run() {
+      if (!isPaused && advTsViewRef.get() != null) {
+        advTsViewRef.get().next();
+        hlUpdt.postDelayed(this, mDuration);
+      }
+    }
+  };
 
-	public Switcher()
-	{}
+  public Switcher() {
+  }
 
-	public Switcher(AdvTextSwitcher view, int duration)
-	{
-		this.advTsView = view;
-		this.mDuration = duration;
-	}
+  public Switcher(AdvTextSwitcher view, int duration) {
+    this.advTsViewRef = new WeakReference<>(view);
+    this.mDuration = duration;
+  }
 
-	public Switcher setDuration(int duration)
-	{
-		this.mDuration = duration;
-		return this;
-	}
+  public Switcher setDuration(int duration) {
+    this.mDuration = duration;
+    return this;
+  }
 
-	public Switcher attach(AdvTextSwitcher view)
-	{
-		this.pause();
-		this.advTsView = view;
-		return this;
-	}
+  public Switcher attach(AdvTextSwitcher view) {
+    this.pause();
+    this.advTsViewRef = new WeakReference<>(view);
+    return this;
+  }
 
-	public void start()
-	{
-		isPaused = false;
-		if (this.advTsView != null)
-		{
-			hlUpdt.postDelayed(rbUpdt, mDuration);
-		}
-	}
+  public void start() {
+    isPaused = false;
+    if (this.advTsViewRef != null) {
+      hlUpdt.postDelayed(rbUpdt, mDuration);
+    }
+  }
 
-	public void pause()
-	{
-		isPaused = true;
-	}
-
-	public Handler hlUpdt = new Handler();
-
-	public Runnable rbUpdt = new Runnable(){
-		@Override
-		public void run()
-		{
-			if (!isPaused && advTsView != null)
-			{
-				advTsView.next();
-				hlUpdt.postDelayed(this, mDuration);
-			}
-		}
-	};
+  public void pause() {
+    isPaused = true;
+  }
 }
